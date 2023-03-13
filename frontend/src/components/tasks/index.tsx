f88475d761cfa4352ddc51c57c35cc396b28972d
SeefatHimel <Seefat.hossain@brainstation-23.com>
2023-03-13 17:47:10 +0600

    added search tasks

--- frontend/src/components/tasks/index.tsx
+++ frontend/src/components/tasks/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Empty, Spin, message } from "antd";
+import { Button, Empty, Spin, message, Input } from "antd";
 import { createContext, useEffect, useState } from "react";
 
 import GlobalModal from "../modals/globalModal";
@@ -9,7 +9,7 @@ import { TaskDto } from "models/tasks";
 import TaskInput from "./components/taskInput";
 import VerticalCard from "./components/verticalCard";
 import { userAPI } from "APIs";
-
+const { Search } = Input;
 export const TaskContext = createContext<any>({
   taskList: [],
   runningTask: null,
@@ -22,7 +22,9 @@ const TasksPage = () => {
   const [warningModalOpen, setWarningModalOpen] = useState<boolean>(false);
   const [warningData, setWarningData] = useState<any>([]);
   const [tasks, setTasks] = useState<TaskDto[]>([]);
+  const [searchedTasks, setSearchedTasks] = useState<TaskDto[]>([]);
   const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
   const [syncing, setSyncing] = useState(false);
   const [selectedTask, setSelectedTask] = useState<TaskDto | null>(null);
   const [runningTask, setRunningTask] = useState<TaskDto | null>(null);
@@ -33,6 +35,7 @@ const TasksPage = () => {
       const res = await userAPI.createTask(data);
       message.success("Task created successfully");
       setTasks((tasks) => [res, ...tasks]);
+      setSearchedTasks((tasks) => [res, ...tasks]);
       if (tasks) {
         tasks.map((task) => {
           if (task.sessions[task.sessions.length - 1]?.status === "STARTED") {
@@ -67,7 +70,10 @@ const TasksPage = () => {
     setLoading(true);
     try {
       const res = await userAPI.deleteTask(taskId);
-      if (res) setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+      if (res) {
+        setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+        setSearchedTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+      }
     } catch (error) {
       message.error("Error deleting task");
     } finally {
@@ -80,6 +86,7 @@ const TasksPage = () => {
     try {
       const res = await userAPI.getTasks();
       setTasks([...res] || []);
+      setSearchedTasks([...res] || []);
     } catch (error) {
       message.error("Error getting tasks");
     } finally {
@@ -91,6 +98,8 @@ const TasksPage = () => {
     try {
       const res = await userAPI.syncTasks();
       setTasks([...res] || []);
+      setSearchedTasks([...res] || []);
+
       message.success("Sync Successful");
     } catch (error) {
       message.error("Error syncing tasks");
@@ -103,6 +112,22 @@ const TasksPage = () => {
   useEffect(() => {
     getTasks();
   }, []);
+
+  useEffect(() => {
+    if (searchText?.length > 0) {
+      setSearchedTasks(
+        tasks.filter((task) => {
+          if (
+            task.title.includes(searchText) ||
+            task.description.includes(searchText)
+          )
+            return task;
+        })
+      );
+    } else {
+      setSearchedTasks(tasks);
+    }
+  }, [searchText]);
 
   useEffect(() => {
     if (tasks) {
@@ -154,7 +179,14 @@ const TasksPage = () => {
                 className="flex flex-col gap-2 overflow-y-auto border-r-2 pr-2"
                 style={{ height: "calc(100vh - 155px)" }}
               >
-                {tasks.map((task) => (
+                <Search
+                  placeholder="input search text"
+                  onChange={(e) => {
+                    setSearchText(e.target.value);
+                  }}
+                  allowClear
+                />
+                {searchedTasks.map((task) => (
                   <VerticalCard
                     key={task.id}
                     task={task}
