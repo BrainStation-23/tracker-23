62f94515b4d1d17e3540fff27dcd74d5c79eb2c9
Shakil Ahmed <shakilahmmeed@gmail.com>
2023-03-24 04:21:19 +0600

    added login page

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,12 +1,12 @@
 import { GetCookie } from "@/services/cookie.service";
 import {
-  Avatar,
-  Button,
-  Dropdown,
-  Menu,
-  MenuProps,
-  message,
-  Space,
+	Avatar,
+	Button,
+	Dropdown,
+	Menu,
+	MenuProps,
+	message,
+	Space,
 } from "antd";
 import LogOutButton from "../logOutButton";
 import { useRouter } from "next/router";
@@ -20,113 +20,113 @@ import { SyncOutlined } from "@ant-design/icons";
 import { userAPI } from "APIs";
 
 function Navbar() {
-  const [userDetails, setUserDetails] = useState<LoginResponseDto>();
-  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
-  const [syncing, setSyncing] = useState(false);
-  const router = useRouter();
-  const path = router.asPath;
-  const btnText = path === "/login" ? "Register" : "Login";
-  const access_token = GetCookie("access_token");
-  const [pageInfo, setPageInfo] = useState([]);
-  useEffect(() => {
-    const tmp = getLocalStorage("userDetails");
-    if (!userDetails && tmp) setUserDetails(tmp);
-  }, [userDetails, path]);
-  const items: MenuProps["items"] = [
-    {
-      key: "1",
-      icon: (
-        <div className="w-[100px]">
-          <Button
-            className={`flex w-full items-center ${
-              syncing ? "border-green-500 text-green-500" : ""
-            }`}
-            onClick={async () => {
-              setSyncing(true);
-              await syncTasks();
-            }}
-          >
-            <SyncOutlined spin={syncing} /> Sync
-          </Button>
-        </div>
-      ),
-    },
-    {
-      key: "2",
-      icon: (
-        <div className="w-[100px]">
-          <LogOutButton />
-        </div>
-      ),
-    },
-  ];
-  const syncTasks = async () => {
-    try {
-      const res = await userAPI.syncTasks();
-      message.success("Sync Successful");
-    } catch (error) {
-      message.error("Error syncing tasks");
-    } finally {
-      setSyncing(false);
-    }
-  };
-  const menuProps = {
-    items,
-    onClick: () => {},
-  };
+	const [userDetails, setUserDetails] = useState<LoginResponseDto>();
+	const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+	const [syncing, setSyncing] = useState(false);
+	const router = useRouter();
+	const path = router.asPath;
+	const btnText = path === "/login" ? "Register" : "Login";
+	const access_token = GetCookie("access_token");
+	const [pageInfo, setPageInfo] = useState([]);
+	useEffect(() => {
+		const tmp = getLocalStorage("userDetails");
+		if (!userDetails && tmp) setUserDetails(tmp);
+	}, [userDetails, path]);
+	const items: MenuProps["items"] = [
+		{
+			key: "1",
+			icon: (
+				<div className="w-[100px]">
+					<Button
+						className={`flex w-full items-center ${
+							syncing ? "border-green-500 text-green-500" : ""
+						}`}
+						onClick={async () => {
+							setSyncing(true);
+							await syncTasks();
+						}}
+					>
+						<SyncOutlined spin={syncing} /> Sync
+					</Button>
+				</div>
+			),
+		},
+		{
+			key: "2",
+			icon: (
+				<div className="w-[100px]">
+					<LogOutButton />
+				</div>
+			),
+		},
+	];
+	const syncTasks = async () => {
+		try {
+			const res = await userAPI.syncTasks();
+			message.success("Sync Successful");
+		} catch (error) {
+			message.error("Error syncing tasks");
+		} finally {
+			setSyncing(false);
+		}
+	};
+	const menuProps = {
+		items,
+		onClick: () => {},
+	};
 
-  const dropdownRender = (menu: React.ReactNode) => (
-    <div className="float-right">{menu}</div>
-  );
-  return (
-    <div className=" mb-2 flex h-16 w-full items-center justify-between px-5">
-      <div className="py-6 text-xl text-blue-500  hover:text-green-500">
-        {sideMenuOptions?.map(
-          (option) =>
-            router.asPath.includes(option.link) && (
-              <div
-                key={Math.random()}
-                className={`flex items-center gap-2 rounded-lg px-1 text-black `}
-              >
-                <div className=" stroke-black">{option.icon}</div>
-                <div className={`text-base font-semibold`}>{option.title}</div>
-              </div>
-            )
-        )}
-        {pageInfo[0] && (
-          <div
-            className={`flex items-center gap-2 rounded-lg px-1 text-black `}
-            onClick={() => {
-              router.push(pageInfo[0].link);
-            }}
-          >
-            <div>{pageInfo[0].icon}</div>
-            <div className={`text-sm`}>{pageInfo[0].title}</div>
-          </div>
-        )}
-      </div>
-      {path === "/login" || path === "/registration" ? (
-        <Button
-          type="primary"
-          danger
-          onClick={() => {
-            router.push(`/${path === "/login" ? "registration" : "login"}`);
-          }}
-        >
-          {btnText}
-        </Button>
-      ) : (
-        <div className="flex items-center gap-4">
-          <div
-            className="flex h-9 w-9 cursor-pointer items-center justify-center"
-            style={{
-              border: "1px solid #ECECED",
-              borderRadius: "8px",
-            }}
-          >
-            <BellIconSvg />
-          </div>
-          {/* <Dropdown
+	const dropdownRender = (menu: React.ReactNode) => (
+		<div className="float-right">{menu}</div>
+	);
+	return (
+		<div className=" mb-2 flex h-16 w-full items-center justify-between px-5">
+			<div className="py-6 text-xl text-blue-500  hover:text-green-500">
+				{sideMenuOptions?.map(
+					(option) =>
+						router.asPath.includes(option.link) && (
+							<div
+								key={Math.random()}
+								className={`flex items-center gap-2 rounded-lg px-1 text-black `}
+							>
+								<div className=" stroke-black">{option.icon}</div>
+								<div className={`text-base font-semibold`}>{option.title}</div>
+							</div>
+						)
+				)}
+				{pageInfo[0] && (
+					<div
+						className={`flex items-center gap-2 rounded-lg px-1 text-black `}
+						onClick={() => {
+							router.push(pageInfo[0].link);
+						}}
+					>
+						<div>{pageInfo[0].icon}</div>
+						<div className={`text-sm`}>{pageInfo[0].title}</div>
+					</div>
+				)}
+			</div>
+			{path === "/login" || path === "/registration" ? (
+				<Button
+					type="primary"
+					danger
+					onClick={() => {
+						router.push(`/${path === "/login" ? "registration" : "login"}`);
+					}}
+				>
+					{btnText}
+				</Button>
+			) : (
+				<div className="flex items-center gap-4">
+					<div
+						className="flex h-9 w-9 cursor-pointer items-center justify-center"
+						style={{
+							border: "1px solid #ECECED",
+							borderRadius: "8px",
+						}}
+					>
+						<BellIconSvg />
+					</div>
+					{/* <Dropdown
             menu={menuProps}
             trigger={["click"]}
             overlayStyle={{
@@ -137,61 +137,61 @@ function Navbar() {
             className="flex w-[300px] items-center rounded bg-gray-50 p-2 hover:bg-gray-100"
           > */}
 
-          <div>
-            <Dropdown
-              menu={menuProps}
-              dropdownRender={dropdownRender}
-              trigger={["click"]}
-              className="duration-1000 delay-1000 transition-all"
-              overlayClassName="duration-1000 delay-1000 transition-all"
-            >
-              <div
-                className="flex w-[300px] items-center justify-between"
-                // onClick={() => {
-                //   !dropdownOpen && setDropdownOpen(true);
-                // }}
-              >
-                <div className="flex items-center gap-2">
-                  {userDetails?.picture ? (
-                    <Avatar
-                      src={userDetails.picture}
-                      alt="N"
-                      className="h-[40px] w-[40px]"
-                    />
-                  ) : (
-                    <Avatar
-                      src={
-                        "https://st3.depositphotos.com/15437752/19006/i/600/depositphotos_190061104-stock-photo-silhouette-male-gradient-background-white.jpg"
-                      }
-                      alt="N"
-                      className="h-[40px] w-[40px]"
-                    />
-                  )}
-                  <div className="flex flex-col text-sm">
-                    <div className="font-semibold">
-                      {userDetails?.firstName} {userDetails?.lastName}
-                    </div>
-                    <div className="font-normal">Project Manager</div>
-                  </div>
-                </div>
-                <div
-                  className="flex h-7 w-7 cursor-pointer items-center justify-center bg-[#ECECED]"
-                  style={{
-                    borderRadius: "8px",
-                  }}
-                >
-                  {dropdownOpen ? <FiChevronUp /> : <FiChevronDown />}
-                </div>
-              </div>
-            </Dropdown>
-          </div>
-          {/* </Dropdown> */}
+					<div>
+						<Dropdown
+							menu={menuProps}
+							dropdownRender={dropdownRender}
+							trigger={["click"]}
+							className="transition-all delay-1000 duration-1000"
+							overlayClassName="duration-1000 delay-1000 transition-all"
+						>
+							<div
+								className="flex w-[300px] items-center justify-between"
+								// onClick={() => {
+								//   !dropdownOpen && setDropdownOpen(true);
+								// }}
+							>
+								<div className="flex items-center gap-2">
+									{userDetails?.picture ? (
+										<Avatar
+											src={userDetails.picture}
+											alt="N"
+											className="h-[40px] w-[40px]"
+										/>
+									) : (
+										<Avatar
+											src={
+												"https://st3.depositphotos.com/15437752/19006/i/600/depositphotos_190061104-stock-photo-silhouette-male-gradient-background-white.jpg"
+											}
+											alt="N"
+											className="h-[40px] w-[40px]"
+										/>
+									)}
+									<div className="flex flex-col text-sm">
+										<div className="font-semibold">
+											{userDetails?.firstName} {userDetails?.lastName}
+										</div>
+										<div className="font-normal">Project Manager</div>
+									</div>
+								</div>
+								<div
+									className="flex h-7 w-7 cursor-pointer items-center justify-center bg-[#ECECED]"
+									style={{
+										borderRadius: "8px",
+									}}
+								>
+									{dropdownOpen ? <FiChevronUp /> : <FiChevronDown />}
+								</div>
+							</div>
+						</Dropdown>
+					</div>
+					{/* </Dropdown> */}
 
-          {/* <LogOutButton /> */}
-        </div>
-      )}
-    </div>
-  );
+					{/* <LogOutButton /> */}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Navbar;
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,19 +1,23 @@
-import SocialLogin from "@/components/login/socialLogin";
-import Link from "next/link";
-import LoginForm from "../../components/login/loginForm";
+import LoginV2 from "@/components/loginV2";
 
 const Login = () => {
-  return (
-    <div className="pt-32">
-      <LoginForm />
-      <div className="mx-auto w-max p-2">
-        {`Don't have an account? ... `}{" "}
-        <Link href="/registration" className="hover:text-blue-500">
-          Register
-        </Link>
-      </div>
-      <SocialLogin />
-    </div>
-  );
+	return (
+		<div>
+			<LoginV2 />
+		</div>
+	);
+
+	// return (
+	//   <div className="pt-32">
+	//     <LoginForm />
+	//     <div className="mx-auto w-max p-2">
+	//       {`Don't have an account? ... `}{" "}
+	//       <Link href="/registration" className="hover:text-blue-500">
+	//         Register
+	//       </Link>
+	//     </div>
+	//     <SocialLogin />
+	//   </div>
+	// );
 };
 export default Login;
