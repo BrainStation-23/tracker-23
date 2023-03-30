e0afea076d65d30f918e3ca0d94846ab5df59ff3
SeefatHimel <Seefat.hossain@brainstation-23.com>
2023-03-30 23:41:39 +0600

    add head bs icon

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
 import Link from "next/link";
 import { menuOptions } from "utils/constants";
 
 export default function Home() {
   return (
     <div className="mx-auto mt-5 w-max">
-      <Head>
-        <link rel="icon" href="/bsIcon.png" />
-      </Head>
       <h1 className="text-3xl font-bold ">Welcome!!</h1>
 
       <h1 className="flex flex-col gap-3 pt-5">
