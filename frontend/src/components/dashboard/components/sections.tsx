1aa31f2f86c8f9923e7b2f77df0176ffecf59179
SeefatHimel <Seefat.hossain@brainstation-23.com>
2023-03-31 11:33:05 +0600

    add spin to dashboard components

--- frontend/src/components/dashboard/components/sections.tsx
+++ frontend/src/components/dashboard/components/sections.tsx
@@ -1,5 +1,15 @@
+import { Spin } from "antd";
+import { useEffect, useState } from "react";
+
 type Props = { children: any; title: string };
 const DashBoardSection = ({ children, title }: Props) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
   return (
     <>
       <div className="relative flex flex-col gap-2">
@@ -7,7 +17,9 @@ const DashBoardSection = ({ children, title }: Props) => {
           <div className="text-2xl font-semibold ">{title}</div>
           {/* <div className=" rounded-lg border-2 p-1 px-2 text-sm">This Week</div> */}
         </div>
-        <div>{children}</div>
+        <Spin spinning={isLoading}>
+          <div>{children}</div>
+        </Spin>
       </div>
     </>
   );
