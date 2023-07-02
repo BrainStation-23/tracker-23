fdadb61165ab760cb6578ce08bfeef09f6f4c261
Sudipta <uadayghosh@gmail.com>
2023-07-03 00:45:11 +0600

    only backend trigger when backed changes

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit  {
 
+  // connection test
   async onModuleInit() {
     await this.$connect();
   }
