ed1b00260cea1dc88a2737e5705c91c46ed9a48b
Sudipta <uadayghosh@gmail.com>
2023-07-03 02:05:08 +0600

    database url updated

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -11,10 +11,11 @@ export class PrismaService extends PrismaClient implements OnModuleInit  {
   }
 
   constructor(config: ConfigService) {
+    console.log('config.get(DATABASE_URL)==========+L>>>>>',config.get('DATABASE_URL'))
     super({
       datasources: {
         db: {
-          url: config.get('DATABASE_URL'),
+          url: 'postgresql://tracker23@tracker23-db:jssbubs23#@tracker23-db.postgres.database.azure.com:5432/tracker23?sslmode=require',
         },
       },
     });
