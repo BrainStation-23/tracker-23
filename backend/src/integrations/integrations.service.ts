671d8bb57657b706022d37df85fcf6c71d91bc64
SeefatHimel <seefathimel1@gmail.com>
2023-07-10 15:47:57 +0600

    update (backend): moved updated integration to integration module

--- backend/src/integrations/integrations.service.ts
+++ backend/src/integrations/integrations.service.ts
@@ -1,10 +1,19 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { lastValueFrom } from 'rxjs';
 import { APIException } from 'src/internal/exception/api.exception';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+import { HttpService } from '@nestjs/axios';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IntegrationType, User } from '@prisma/client';
+
 @Injectable()
 export class IntegrationsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+    private httpService: HttpService,
+  ) {}
 
   async getIntegrations(user: User) {
     return await this.prisma.integration.findMany({
@@ -17,6 +26,39 @@ export class IntegrationsService {
         accessToken: true,
       },
     });
+  }
+
+  async updateIntegration(user: User, integrationID: number) {
+    const tokenUrl = 'https://auth.atlassian.com/oauth/token';
+    const headers: any = { 'Content-Type': 'application/json' };
+    const integration = await this.prisma.integration.findFirst({
+      where: { userId: user.id, type: IntegrationType.JIRA, id: integrationID },
+    });
+    if (!integration) {
+      return null;
+    }
+
+    const data = {
+      grant_type: 'refresh_token',
+      client_id: this.config.get('JIRA_CLIENT_ID'),
+      client_secret: this.config.get('JIRA_SECRET_KEY'),
+      refresh_token: integration?.refreshToken,
+    };
+
+    const tokenResp = (
+      await lastValueFrom(this.httpService.post(tokenUrl, data, headers))
+    ).data;
+
+    const updated_integration =
+      integration &&
+      (await this.prisma.integration.update({
+        where: { id: integration?.id },
+        data: {
+          accessToken: tokenResp.access_token,
+          refreshToken: tokenResp.refresh_token,
+        },
+      }));
+    return updated_integration;
   }
 
   async deleteIntegration(user: User, integrationId: number) {
