2e40069bc0af1ebbccb0d777e0f6dbda3cd4a344
DipuBala <dipankar.bala@brainstation-23.com>
2023-07-18 20:06:31 +0600

    sync api  can update with jira

---
+++ backend/prisma/migrations/20230718083535_parent_task_added/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "jiraUpdatedAt" TIMESTAMP(3),
+ADD COLUMN     "parentTaskId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_parentTaskId_fkey" FOREIGN KEY ("parentTaskId") REFERENCES "Task"("id") ON DELETE SET NULL ON UPDATE CASCADE;
