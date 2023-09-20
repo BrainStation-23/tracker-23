51a547bf5e4d99928295c0bdd8d076c6a9935378
SeefatHimel <seefathimel1@gmail.com>
2023-09-20 14:30:11 +0600

    fix (frontend): login page

--- frontend/src/components/auth/components/authLeftPanel.tsx
+++ frontend/src/components/auth/components/authLeftPanel.tsx
@@ -6,7 +6,7 @@ import LeftPanelTopPicture from "./leftPanelTopPicture";
 const AuthLeftPanel = () => {
   return (
     <div className=" bg-[url('/images/bg_login.svg')] bg-cover bg-no-repeat">
-      <div className="relative ml-[8%] flex h-full w-full flex-col-reverse">
+      <div className="relative ml-[8%] flex h-full w-full flex-col">
         <div className="absolute right-0 top-[8%] flex h-1/2 w-full justify-end ">
           {/* <Image
             alt=".."
@@ -19,14 +19,14 @@ const AuthLeftPanel = () => {
           /> */}
           <LeftPanelTopPicture />
         </div>
-        <div className=" z-10 mb-[32%] flex flex-col gap-6 lg:mb-[22%] 2xl:mb-[25%]">
+        <div className=" z-10 mt-[32%] flex flex-col gap-6 lg:mt-[72%] 2xl:mt-[55%]">
           <div className=" w-3/4 text-3xl font-semibold text-white xl:text-5xl 2xl:text-6xl">
             Have you heard about Tracker23 Tasks?
           </div>
           <div className="text-white  2xl:text-2xl">
             Sign up or log in to start tracking your time
           </div>
-          <Button className="mt-12  2xl:text-2xl flex w-min items-center rounded-lg bg-white py-6 px-3 font-semibold">
+          <Button className="mt-12  flex w-min items-center rounded-lg bg-white py-6 px-3 font-semibold 2xl:text-2xl">
             Try Tracker23 Now
           </Button>
         </div>
