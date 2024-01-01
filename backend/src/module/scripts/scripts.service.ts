1a799b8bcaf37513dc008520a620d868c2e321e3
Seefat Hossain Himel <seefathimel1@gmail.com>
2024-01-01 16:00:39 +0600

    add (backend) : add migration script

--- backend/src/module/scripts/scripts.service.ts
+++ backend/src/module/scripts/scripts.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
-import { TasksService } from '../tasks/tasks.service';
 import { IntegrationDatabase } from 'src/database/integrations';
 import { ProjectDatabase } from 'src/database/projects';
+
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { Scripts } from '@prisma/client';
+
+import { APIException } from '../exception/api.exception';
+import { PrismaService } from '../prisma/prisma.service';
+import { TasksService } from '../tasks/tasks.service';
 
 @Injectable()
 export class ScriptsService {
@@ -102,6 +106,118 @@ export class ScriptsService {
           }
         }
       }
+    }
+  }
+
+  async migrateOutlookSchema() {
+    try {
+      let scriptStatus: Scripts | null = await this.prisma.scripts.findUnique({
+        where: { key: 'outlook-migration-1' },
+      });
+      console.log(
+        '🚀 ~ file: scripts.service.ts:117 ~ ScriptsService ~ migrateOutlookSchema ~ scriptStatus:',
+        scriptStatus,
+      );
+      if (!scriptStatus) {
+        await this.prisma.scripts.create({
+          data: {
+            key: 'outlook-migration-1',
+            title: 'Outlook Migration',
+            description: 'Outlook Migration',
+          },
+        });
+        scriptStatus = await this.prisma.scripts.findUnique({
+          where: { key: 'outlook-migration-1' },
+        });
+      }
+      console.log(
+        '🚀 ~ file: scripts.service.ts:132 ~ ScriptsService ~ migrateOutlookSchema ~ scriptStatus:',
+        scriptStatus,
+      );
+      if (scriptStatus?.done)
+        return { message: 'Outlook Migration already done! ' };
+      // update wrong integrations
+      await this.prisma.integration.updateMany({
+        where: {
+          type: {
+            not: {
+              in: ['JIRA', 'TRACKER23', 'OUTLOOK'],
+            },
+          },
+        },
+        data: {
+          type: 'JIRA',
+        },
+      });
+
+      await this.prisma.task.updateMany({
+        where: {
+          source: {
+            not: {
+              in: ['JIRA', 'TRACKER23', 'OUTLOOK'],
+            },
+          },
+        },
+        data: {
+          source: 'TRACKER23',
+          dataSource: 'Tracker 23',
+        },
+      });
+
+      const allIntegrations = await this.prisma.integration.findMany({
+        include: {
+          Projects: true,
+        },
+      });
+
+      for (const integration of allIntegrations) {
+        const projectids = [];
+        for (let i = 0; i < integration?.Projects?.length; i++) {
+          projectids.push(integration?.Projects[i].id);
+        }
+        await this.prisma.task.updateMany({
+          where: {
+            projectId: { in: projectids },
+          },
+          data: {
+            source: integration.type,
+            dataSource: integration.site ? integration.site : '',
+          },
+        });
+      }
+      await this.prisma.scripts.update({
+        where: { key: 'outlook-migration-1' },
+        data: {
+          done: true,
+        },
+      });
+      return { message: 'Outlook Migration Successful' };
+    } catch (error) {
+      console.log(
+        '🚀 ~ file: scripts.service.ts:118 ~ ScriptsService ~ migrateOutlookSchema ~ error:',
+        error,
+      );
+      try {
+        await this.prisma.scripts.update({
+          where: { key: 'outlook-migration-1' },
+          data: {
+            done: false,
+          },
+        });
+      } catch (error) {
+        console.log(
+          '🚀 ~ file: scripts.service.ts:128 ~ ScriptsService ~ migrateOutlookSchema ~ error:',
+          error,
+        );
+        throw new APIException(
+          'Migration Failed - Failed to upgrade outlook-migration',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+      throw new APIException(
+        'Migration Failed',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
