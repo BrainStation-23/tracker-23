0c83774e15f2116cef516e1cea241aabc4d47f46
Md. Belal Hossain <belal.cseai@gmail.com>
2024-01-11 20:06:10 +0600

    FE (Update): Updates Task List component

--- frontend/src/components/report/singleReports/taskListReport.tsx
+++ frontend/src/components/report/singleReports/taskListReport.tsx
@@ -3,60 +3,41 @@ import { getFormattedTasks } from "@/services/taskActions";
 import { ReportData } from "@/storage/redux/reportsSlice";
 import { userAPI } from "APIs";
 import { IntegrationType } from "models/integration";
-import {
-  ReportPageTabs,
-  SprintReportDto,
-  SprintUser,
-  SprintUserReportDto,
-} from "models/reports";
+import { SprintUser } from "models/reports";
 import { TaskDto } from "models/tasks";
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import ReportWrapper from "../components/reportWrapper";
 import TypeDependentSection from "../components/typeDependentSection";
 import TopPanelTaskListComponents from "../components/topPanelTaskListComponents";
 import TaskListReportComponent from "../components/taskListReportComponent";
 import { ExcelExport } from "@/services/exportHelpers";
-import { message } from "antd";
+import { Button, Spin, message } from "antd";
+import ReportHeaderComponent from "../components/reportHeaderComponent";
+import UserSelectorComponent from "@/components/common/topPanels/components/userSelector";
+import { LuDownload } from "react-icons/lu";
 
 type Props = {
   reportData: ReportData;
 };
+
 export default function TaskListReport({ reportData }: Props) {
-  const dispatch = useDispatch();
-  // const [isLoading, setIsLoading] = useState(false);
-  const [Loaded, setLoaded]: any = useState({});
-  const [SprintReportFecthedOnce, setSprintReportFecthedOnce] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [downloading, setDownloading] = useState<boolean>(false);
-  const [data, setData] = useState([]);
   const [tasks, setTasks] = useState<TaskDto[]>([]);
-  const [sprintEstimateReportData, setSprintEstimateReportData] =
-    useState<SprintUserReportDto>();
   const [selectedSource, setSelectedSource] = useState<IntegrationType[]>();
-  const [sprintReportData, setSprintReportData] = useState<SprintReportDto>();
   const [sprints, setSprints] = useState<number[]>([]);
-  const [sprintReportSprintId, setSprintReportSprintId] = useState<number>();
   const [users, setUsers] = useState<SprintUser[]>([]);
   const [projects, setProjects] = useState<number[]>([]);
   const [calendarIds, setCalendarIds] = useState<number[]>([]);
-  const [projectSprintReport, setProjectSprintReport] = useState<number>();
-  const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
   const [selectedUser, setSelectedUser] = useState<number>();
   const [dateRange, setDateRange] = useState(getDateRangeArray("this-week"));
 
-  const [dateRangeArray, setDateRangeArray] = useState([]);
-  const [column, setColumns] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [status, setStatus] = useState([]);
   const [priority, setPriority] = useState([]);
-  const [activeTab, setActiveTab] = useState<ReportPageTabs>(
-    "Time Sheet"
-    // "Sprint Report"
-  );
+  const activeTab = "Task List";
 
   const getTaskListReport = async () => {
-    // setIsLoading(true);
-    Loaded["Task List"] = false;
+    setIsLoading(true);
     const res = await userAPI.getTaskListReport({
       searchText,
       selectedDate: dateRange,
@@ -71,83 +52,38 @@ export default function TaskListReport({ reportData }: Props) {
       const { formattedTasks } = getFormattedTasks(res);
       setTasks(formattedTasks || []);
     }
-    // setIsLoading(false);
-    Loaded["Task List"] = true;
+    setIsLoading(false);
   };
 
   const excelExport = async () => {
     setDownloading(true);
-    if (activeTab === "Task List") {
-      try {
-        const res = await userAPI.exportTasks({
-          searchText,
-          selectedDate: dateRange,
-          priority,
-          status,
-          sprints,
-          types: selectedSource,
-          projectIds: projects,
-          userIds: [selectedUser],
-          calendarIds,
-        });
-        console.log(
-          "🚀 ~ file: topPanelExportPage.tsx:54 ~ excelExport ~ res:",
-          res
+    try {
+      const res = await userAPI.exportTasks({
+        searchText,
+        selectedDate: dateRange,
+        priority,
+        status,
+        sprints,
+        types: selectedSource,
+        projectIds: projects,
+        userIds: [selectedUser],
+        calendarIds,
+      });
+      console.log(
+        "🚀 ~ file: topPanelExportPage.tsx:54 ~ excelExport ~ res:",
+        res
+      );
+      if (!res) {
+        message.error(
+          res?.error?.message ? res?.error?.message : "Export Failed"
         );
-        if (!res) {
-          message.error(
-            res?.error?.message ? res?.error?.message : "Export Failed"
-          );
-        } else {
-          ExcelExport({ file: res, name: "Tracker 23 TaskList Report" });
-        }
-      } catch (error) {
-        message.error("Export Failed");
+      } else {
+        ExcelExport({ file: res, name: `${reportData.name}` });
       }
+    } catch (error) {
+      message.error("Export Failed");
     }
-    if (activeTab === "Sprint Estimate") {
-      try {
-        const res = await userAPI.exportSprintReport({
-          sprints,
-          selectedUsers,
-          projectIds: projects,
-        });
-        if (!res) {
-          message.error(
-            res?.error?.message ? res?.error?.message : "Export Failed"
-          );
-        } else {
-          ExcelExport({ file: res, name: "Tracker 23 Sprint Report" });
-        }
-      } catch (error) {
-        message.error("Export Failed");
-      }
-    }
-    if (activeTab === "Time Sheet") {
-      try {
-        const res = await userAPI.exportTimeSheetReport({
-          startDate: dateRange[0],
-          endDate: dateRange[1],
-          userIds: selectedUsers,
-          types: selectedSource,
-          projectIds: projects,
-          calendarIds,
-        });
-        console.log(
-          "🚀 ~ file: topPanelExportPage.tsx:54 ~ excelExport ~ res:",
-          res
-        );
-        if (!res) {
-          message.error(
-            res?.error?.message ? res?.error?.message : "Export Failed"
-          );
-        } else {
-          ExcelExport({ file: res, name: "Tracker 23 Time Sheet Report" });
-        }
-      } catch (error) {
-        message.error("Export Failed");
-      }
-    }
+
     setDownloading(false);
   };
 
@@ -166,36 +102,28 @@ export default function TaskListReport({ reportData }: Props) {
 
   return (
     <div>
-      <ReportWrapper
-        title="Time Reports"
-        isLoading={!Loaded[activeTab]}
-        {...{
-          setDateRange,
-          dateRange,
-
-          activeTab,
-          setActiveTab,
-          sprints,
-          setSprints,
-          projects,
-          setProjects,
-          sprintEstimateReportData,
-          selectedUsers,
-          setSelectedUsers,
-          users,
-          selectedUser,
-          setSelectedUser,
-          downloading,
-          excelExport,
-        }}
-        datePicker={
+      <ReportHeaderComponent
+        title={reportData.name}
+        exportButton={
+          <Button
+            type="ghost"
+            className="flex items-center gap-2 rounded-md bg-[#016C37] py-4 text-white hover:bg-[#1d8b56] hover:text-white"
+            icon={<LuDownload className="text-xl" />}
+            loading={downloading}
+            onClick={() => excelExport()}
+          >
+            Export to Excel
+          </Button>
+        }
+      >
+        <>
           <DateRangePicker
             selectedDate={dateRange}
             setSelectedDate={setDateRange}
           />
-        }
-        typeSelector={
+
           <TypeDependentSection
+            config={reportData?.config}
             {...{
               activeTab,
               selectedSource,
@@ -208,26 +136,26 @@ export default function TaskListReport({ reportData }: Props) {
               setCalendarIds,
             }}
           />
-        }
-        topPanelComponent={
-          <>
-            {activeTab === "Task List" && (
-              <TopPanelTaskListComponents
-                {...{
-                  searchText,
-                  setSearchText,
-                  status,
-                  setStatus,
-                  priority,
-                  setPriority,
-                }}
-              />
-            )}
-          </>
-        }
-      >
+          <TopPanelTaskListComponents
+            {...{
+              searchText,
+              setSearchText,
+              status,
+              setStatus,
+              priority,
+              setPriority,
+            }}
+          />
+
+          <UserSelectorComponent
+            {...{ userList: users, selectedUser, setSelectedUser }}
+            className="w-[210px]"
+          />
+        </>
+      </ReportHeaderComponent>
+      <Spin className="custom-spin" spinning={isLoading}>
         <TaskListReportComponent {...{ tasks }} />
-      </ReportWrapper>
+      </Spin>
     </div>
   );
 }
