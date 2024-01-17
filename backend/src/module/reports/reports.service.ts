f3b78b43eace9600c15d867c1cdbb8278612fd8c
DipuBala <dipankar.bala@brainstation-23.com>
2024-01-17 15:24:47 +0600

    BE: comment removed

--- backend/src/module/reports/reports.service.ts
+++ backend/src/module/reports/reports.service.ts
@@ -60,21 +60,6 @@ export class ReportsService {
     id: number,
     query: UpdateReportDto,
   ): Promise<Report | null> {
-    // const sprintIds = query.sprintIds as unknown as string;
-    // const sprintIdArray =
-    //   sprintIds && sprintIds.split(',').map((item) => Number(item.trim()));
-
-    // const projectIds = query.projectIds as unknown as string;
-    // const projectIdArray =
-    //   projectIds && projectIds.split(',').map((item) => Number(item.trim()));
-
-    // const types = query.types as unknown as string;
-    // const typeArray = types && types.split(',');
-
-    // const userIds = query.userIds as unknown as string;
-    // const userIdArray =
-    //   userIds && userIds.split(',').map((item) => Number(item.trim()));
-
     const reqConfigBody = {
       ...(query.startDate && {
         startDate: query.startDate,
