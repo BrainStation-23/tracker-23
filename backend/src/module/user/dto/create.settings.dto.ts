adc589ed445f8f655cacc8d30a9ef70bad011586
DipuBala <dipankar.bala@brainstation-23.com>
2024-01-19 19:11:17 +0600

    BE: Settings API updated

--- backend/src/module/user/dto/create.settings.dto.ts
+++ backend/src/module/user/dto/create.settings.dto.ts
@@ -1,8 +1,12 @@
-import { IsOptional } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateSettingsReqDto {
   @IsOptional()
   syncTime: 1 | 6 | 12;
   @IsOptional()
   timeFormat: 'Day' | 'Hour';
+
+  @IsOptional()
+  @IsNumber()
+  extraSpent: number;
 }
