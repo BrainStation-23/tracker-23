bcc3b6b6ae765a9197f07e238ed52f3f206132d5
Seefat Hossain Himel <seefathimel1@gmail.com>
2024-02-06 12:27:38 +0600

    add (frontend) : ts support

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,13 @@ module.exports = withTM({
 
     return config;
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   output: "standalone",
   images: {
     unoptimized: true, // Disable Image Optimization API for next export
