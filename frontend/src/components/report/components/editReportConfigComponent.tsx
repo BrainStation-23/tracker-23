c80324a094d9679b77837e0b49d77d43e438d791
Md. Belal Hossain <belal.cseai@gmail.com>
2024-03-15 16:32:04 +0600

    adds new component for edit report config slice

---
+++ frontend/src/components/report/components/editReportConfigComponent.tsx
@@ -0,0 +1,23 @@
+import { ReportData, setReportInEditSlice } from "@/storage/redux/reportsSlice";
+import { Button } from "antd";
+import React from "react";
+import { useDispatch } from "react-redux";
+
+export default function EditReportConfigComponent({
+  reportData,
+  title,
+}: {
+  reportData: ReportData;
+  title?: string;
+}) {
+  const dispatch = useDispatch();
+  function handleEdit() {
+    dispatch(setReportInEditSlice(reportData));
+  }
+
+  return (
+    <Button type="primary" onClick={handleEdit}>
+      {title ?? "Edit Report"}
+    </Button>
+  );
+}
