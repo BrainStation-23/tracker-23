b7065eb516cc1425675640b986c087ef2b08eee6
DipuBala <dipankar.bala@brainstation-23.com>
2024-03-20 15:45:54 +0600

    BE: RabbitMQ set-up added

--- backend/config/queue/index.ts
+++ backend/config/queue/index.ts
@@ -1,5 +1,7 @@
-const { AMQP_URL } = process.env;
+const { AMQP_URL, RABBITMQ_DEFAULT_USER, RABBITMQ_DEFAULT_PASS } = process.env;
 
 export const queueConfig = {
-  amqp_url: AMQP_URL || 'amqp://localhost',
+  amqp_url: AMQP_URL || 'localhost:1234',
+  userName: RABBITMQ_DEFAULT_USER || 'admin',
+  password: RABBITMQ_DEFAULT_PASS || 'password',
 };
