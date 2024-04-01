ef6bf1874eb77eac9aed359ae59be0f14336e2c7
Md. Belal Hossain <belal.cseai@gmail.com>
2024-04-01 19:26:29 +0600

    FE: updates dynamic page slot in page level

--- frontend/src/pages/reports/[reportPageId].tsx
+++ frontend/src/pages/reports/[reportPageId].tsx
@@ -1,7 +1,21 @@
 import ReportPageComponent from "@/components/report/reportPage";
+import { useRouter } from "next/router";
 
 const ReportPage = () => {
-  return <ReportPageComponent />;
+  const router = useRouter();
+  let pageId: number | null = null;
+  try {
+    pageId = router.query?.reportPageId
+      ? parseInt(router.query?.reportPageId as string)
+      : null;
+  } catch (e) {
+    router.replace("/reports/");
+  }
+
+  if (!pageId) {
+    router.replace("/reports/");
+  }
+  return <ReportPageComponent pageId={pageId} />;
 };
 
 export default ReportPage;
