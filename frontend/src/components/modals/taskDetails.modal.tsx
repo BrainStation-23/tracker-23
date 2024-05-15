326979a90df5b19d89e431b667bac941cbaf1ba7
Md. Belal Hossain <belal.hossain@gmail.com>
2024-05-15 14:39:31 +0600

    fixes BN-44-Status mismatch between list and detail page

--- frontend/src/components/modals/taskDetails.modal.tsx
+++ frontend/src/components/modals/taskDetails.modal.tsx
@@ -160,7 +160,7 @@ const TaskDetailsModal = ({
                     statusBorderColorEnum[taskDetails?.statusCategoryName],
                 }}
               />
-              <div>{taskStatusEnum[taskDetails?.statusCategoryName]}</div>
+              <div>{taskDetails?.status}</div>
             </div>
           </div>
 
