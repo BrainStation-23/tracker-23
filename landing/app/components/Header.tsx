7a6bb22e7171b273b56e2109b1d6fa70d0ed40f6
asaduzzaman <asaduzzaman@brainstation-23.com>
2024-05-17 18:12:05 +0600

    landing page logo change

--- landing/app/components/Header.tsx
+++ landing/app/components/Header.tsx
@@ -1,25 +1,26 @@
 import Image from "next/image";
-import Logo from "@/public/header/logo_black.png";
+import Logo from "@/public/header/logo.png";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="w-full bg-[url('/header/landing.png')] bg-no-repeat bg-cover">
-      <div className="container mx-auto py-10 px-4 md:px-0">
-        <div className="flex justify-between items-center">
-          <Image src={Logo} alt="Logo" />
+      <div className="container mx-auto pt-4 pb-10 px-4 md:px-0">
+        <div className="flex items-center justify-between">
+          <Image src={Logo} alt="Logo" width={250} />
           <div className="flex gap-1 text-lg font-medium">
-            <a
+            <Link
               href="https://app.timetracker23.com/registration"
               className="text-[#4A3B5C] px-2 md:px-7 py-1 rounded-full hover:text-white hover:bg-main"
             >
               Sign Up
-            </a>
-            <a
+            </Link>
+            <Link
               href="https://app.timetracker23.com/login"
               className="bg-main px-4 md:px-7 py-1 rounded-full text-white"
             >
               Sign In
-            </a>
+            </Link>
           </div>
         </div>
         <div className="md:pl-28 py-20 md:py-40 w-full md:w-2/5 text-center md:text-start">
@@ -30,12 +31,12 @@ export default function Header() {
             One click to track, manage, and elevate your tasks to new heights
             with our software.
           </p>
-          <a
+          <Link
             href="https://app.timetracker23.com"
             className="bg-main w-2/3 px-10 py-1.5 rounded-full text-white"
           >
             GET STARTED
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Pricing from "./components/Pricing";
+// import Pricing from "./components/Pricing";
 import Features from "./components/Features";
 import Suitables from "./components/Suitables";
 import Solutions from "./components/Solutions";
