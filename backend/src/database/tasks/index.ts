9d7d0d0a10d168f0bfbd55b40a91a8a21c168fad
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2024-10-16 20:16:41 +0600

    add fetchTaskByDateRange servie in TaskDatabase

--- backend/src/database/tasks/index.ts
+++ backend/src/database/tasks/index.ts
@@ -51,7 +51,35 @@ export class TasksDatabase {
       },
     });
   }
+  async fetchTasksByDateRange(
+    projectIds: number[],
+    startDate: Date,
+    endDate: Date,
+  ) {
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        projectId: {
+          in: projectIds,
+        },
+        OR: [
+          {
+            createdAt: {
+              gte: startDate,
+              lte: endDate,
+            },
+          },
+          {
+            updatedAt: {
+              gte: startDate,
+              lte: endDate,
+            },
+          },
+        ],
+      },
+    });
 
+    return tasks;
+  }
   async getProject(query: Record<string, any>): Promise<any> {
     try {
       return await this.prisma.project.findFirst({
