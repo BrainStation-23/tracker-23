738fe9b5df2c53ced7a8fbd269c8fd6f178d61f2
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2024-10-24 13:22:29 +0600

    add helper function to find today and yesterday task

--- backend/src/utils/helper/helper.ts
+++ backend/src/utils/helper/helper.ts
@@ -1,3 +1,5 @@
+import * as dayjs from 'dayjs';
+
 export function formatSpentHour(totalHours: number): string {
   const hours = Math.floor(totalHours);
   const minutes = Math.round((totalHours - hours) * 60);
@@ -9,4 +11,27 @@ export function formatSpentHour(totalHours: number): string {
   const hourPart = hours > 0 ? `${hours} hr${hours > 1 ? 's' : ''}` : '';
   const minutePart = minutes > 0 ? `${minutes} m` : '';
   return [hourPart, minutePart].filter(Boolean).join(' ').trim();
+}
+
+export function doesTodayTask(time: number, task: any) {
+  const sessions = task?.sessions;
+  const parsedTime = dayjs(new Date(time));
+  const startTime = parsedTime.startOf('day').valueOf();
+  const endTime = parsedTime.endOf('day').valueOf();
+  if (
+    new Date(task.jiraUpdatedAt ?? task.updatedAt).getTime() >= startTime &&
+    new Date(task.jiraUpdatedAt ?? task.updatedAt).getTime() <= endTime
+  ) {
+    return true;
+  }
+  for (let index = 0, len = sessions.length; index < len; index++) {
+    const session = sessions[index];
+    if (
+      new Date(session.startTime).getTime() >= startTime &&
+      new Date(session.startTime).getTime() <= endTime
+    ) {
+      return true;
+    }
+  }
+  return false;
 }
