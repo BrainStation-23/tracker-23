e0d490aef4d43ce0b2e2e82b195ee83e78b7b8d2
abir.hussain@brainstation-23.com <abir.hussain@brainstation-23.com>
2024-11-07 12:06:25 +0600

    add rewrites function in next.config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,15 @@ const nextConfig = {
   transpilePackages: ["@amcharts/amcharts4"],
   output: "standalone",
   images: {
-    unoptimized: true, // Disable Image Optimization API for next export
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ];
   },
 };
 
