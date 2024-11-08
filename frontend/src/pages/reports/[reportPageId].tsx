7bd2b279cb20ff556f6343670958a6f2a9a64959
DipuBala <dipankar.bala@brainstation-23.com>
2024-11-08 13:22:13 +0600

    Report page  render issue

--- frontend/src/pages/reports/[reportPageId].tsx
+++ frontend/src/pages/reports/[reportPageId].tsx
@@ -1,11 +1,20 @@
 import ReportPageComponent from "@/components/report/reportPage";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const ReportPage = () => {
   const router = useRouter();
-  const pageId: number = router.query.reportPageId
-    ? Number(router.query.reportPageId)
-    : undefined;
+  const [pageId, setPageId] = useState(
+    router.query.reportPageId ? Number(router.query.reportPageId) : undefined
+  );
+
+  useEffect(() => {
+    if (router.isReady) {
+      setPageId(Number(router.query.reportPageId));
+    }
+  }, [router]);
+
+  if (!router.isReady) return null;
 
   if (!pageId) {
     router.replace("/reports");
